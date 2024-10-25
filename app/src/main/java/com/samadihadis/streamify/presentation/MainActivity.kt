971de4cd3104a9e3b492167516518e9bca4653de
Samadihadis <samadi.hadis@gmail.com>
2024-10-25 15:17:39 +0330

    Complete MainActivity

--- app/src/main/java/com/samadihadis/streamify/presentation/MainActivity.kt
+++ app/src/main/java/com/samadihadis/streamify/presentation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.samadihadis.streamify.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -9,16 +10,25 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.samadihadis.streamify.R
 import com.samadihadis.streamify.databinding.ActivityMainBinding
+import com.samadihadis.streamify.utils.SharePreferencesManager
+import com.samadihadis.streamify.utils.SharePreferencesManager.Companion.IS_DARK_MODE_ENABLE
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val storage by lazy {
+        SharePreferencesManager(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupNavigation()
+        setupTheme()
+    }
 
+    private fun setupNavigation() {
         val navView: BottomNavigationView = binding.bottomNavigationView
         val navController = findNavController(R.id.navMainFragment)
         val appBarConfiguration = AppBarConfiguration(
@@ -27,4 +37,16 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
+    private fun setupTheme() {
+        val isDarkModeEnabled = storage.retrieveBoolean(IS_DARK_MODE_ENABLE)
+        AppCompatDelegate.setDefaultNightMode(
+            if (isDarkModeEnabled) {
+                AppCompatDelegate.MODE_NIGHT_YES
+            } else {
+                AppCompatDelegate.MODE_NIGHT_NO
+            }
+        )
+    }
+
 }
