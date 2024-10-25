bb2c42d36a68e0ad86828ca2cab18795983d9988
Samadihadis <samadi.hadis@gmail.com>
2024-10-25 14:46:55 +0330

    Move MainActivity

--- app/src/main/java/com/samadihadis/streamify/presentation/MainActivity.kt
+++ app/src/main/java/com/samadihadis/streamify/presentation/MainActivity.kt
@@ -1,10 +1,7 @@
-package com.samadihadis.streamify
+package com.samadihadis.streamify.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.samadihadis.streamify.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
